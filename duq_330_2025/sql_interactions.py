ca69b27046ec00092e5e13ea45c321d9755fcf7e
brambleyq <quentinlbrambley@gmail.com>
2025-02-26 22:24:27 -0500

    creates a sqlite database

---
+++ duq_330_2025/sql_interactions.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sqlite3
+import sqlalchemy
+
+def create_database():
+    """create a new sqlite database
+    """
+    query = """
+    CREATE TABLE patents (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title VARCHAR(250) NOT NULL
+    );
+    """
+    # Connection is the connection to the database rather than the 
+    # database itself
+    conn = sqlite3.connect('data/patent_npi_db.sqlite')
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    cursor.execute("SELECT sqlite_version();")
+    record = cursor.fetchall()
+    print(record)
+    cursor.close()
+
+if __name__ == "__main__":
+    create_database()
